9d8e5292fc5d940f0e5d2dfe76181085731f8b63
Angel <angel1200z@hotmail.com>
2025-03-17 19:08:16 -0500

    arreglando pakkcge

---
+++ astro.config.mjs
@@ -0,0 +1,13 @@
+import { defineConfig } from 'astro/config';
+
+import vue from '@astrojs/vue';
+
+// https://astro.build/config
+export default defineConfig({
+  base:'/',
+  outDir: 'dist',
+  integrations: [vue({
+    jsx:true,
+    devtools:true
+  })]
+});
