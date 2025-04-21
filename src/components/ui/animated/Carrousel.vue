ae13dd978aa70574514b17f40086ccb4329b8cc9
Angel <angel1200z@hotmail.com>
2025-04-21 14:50:52 -0500

    cambiando foto de pergil

---
+++ src/components/ui/animated/Carrousel.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+import Carousel from "primevue/carousel";
+import CardProyecto from "../CardProyecto.astro";
+defineProps<{
+  items: Array<any>;
+}>();
+</script>
+<template>
+  <Carousel :value="items">
+    <template #item="slotProp">
+      <slot />
+    </template>
+  </Carousel>
+</template>
